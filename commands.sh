5dcf41d1e51e132be3a584493c0ad4c74fcf773c
PKNgit <104201435+PKNgit@users.noreply.github.com>
2022-05-21 22:29:13 +0530

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 apt-get install vim -y
-apt-get install git -y
-git clone https://github.com/PKNgit/Gamutguruspkn.git 
-apt-get install maven -y
-mvn install -f bashfiles/Gamutguruspkn/pom.xml
 apt-get install openssh-server -y
 apt-get install nginx -y
+touch /var/www/html/inde.html
 ENTRYPOINT service ssh start && bash
 ENTRYPOINT service nginx start && bash
