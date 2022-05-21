049a6dead2658a27ffbd155ddb3515211e04937d
PKNgit <104201435+PKNgit@users.noreply.github.com>
2022-05-21 22:42:48 +0530

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -2,6 +2,6 @@
 apt-get install vim -y
 apt-get install openssh-server -y
 apt-get install nginx -y
-touch /var/www/html/inde.html
-ENTRYPOINT service ssh start && bash
-ENTRYPOINT service nginx start && bash
+touch /var/www/html/index.html
+service ssh start && bash
+service nginx start && bash
